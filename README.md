ea21e56caf0f5ed9dac78df761f84d5fb1f67c4f
Deepu Nair <deepu.velappannair@ge.com>
2017-09-20 17:00:22 -0500

    Gulp task to update and start selenium server

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # protractor-jasmine
+
+Command to set test env : export env= qa | dev |local
+Command to start webdriver server webdriver-manager start
+Command to run test using protractor command protractor protractor.conf.conf
+Command to run test from gulp file : gulp test
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,28 @@
-var gulp = require('gulp');
-var protractor = require("gulp-protractor").protractor;
+const gulp = require('gulp');
+const protractor = require("gulp-protractor").protractor;
+const webdriver_update = require('gulp-protractor').webdriver_update;
+const webdriver_standalone = require('gulp-protractor').webdriver_standalone;
+const util = require('gulp-util');
+const runSequence = require('run-sequence');
 
-gulp.task('test',function(){
+gulp.task('webdriver_update',webdriver_update);
+gulp.task('webdriver_standalone',webdriver_update);
+gulp.task('e2e', ['webdriver_update','webdriver_standalone'],function () {
+    log('end to end test started ...');
     gulp.src('./specs/**/*.js')
-    .pipe(protractor({
-        configFile: 'protractor.conf.js'
-    }))
+        .pipe(protractor({
+            configFile: 'protractor.conf.js'
+        }))
 });
+
+function log(msg) {
+    if (typeof (msg) === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                util.log(util.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        util.log(util.colors.blue(msg));
+    }
+}
